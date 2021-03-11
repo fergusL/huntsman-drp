8571b6c9022ea887d8641db81053f0bd94a69b6f
danjampro <danjampro@sky.com>
2021-03-11 11:28:10 +1100

    Docker updates (#79)
    * first commit
    
    * readme
    
    * add wcs to fake data
    
    * update docker files
    
    * update docker files

--- docker/bash-config.sh
+++ docker/bash-config.sh
@@ -1,11 +1,9 @@
 # Setup the LSST bash env
 source ${LSST_HOME}/loadLSST.bash
-# Use the EUPS package manager to install modules
+
+# Use the EUPS package manager to set up required packages
 eups declare obs_huntsman v1 -r "${LSST_HOME}/obs_huntsman"
 eups declare huntsman_drp v1 -r "${LSST_HOME}/huntsman-drp"
 setup obs_huntsman v1
 setup huntsman_drp v1
 setup display_firefly
-# Unfortunately we have to chown the mount point
-# NB: this might cause problems if the screener needs this mounted as well
-sudo chown -R ${USER} ${HUNTSMAN_MOUNT}
--- scripts/testing/run-local-tests.sh
+++ scripts/testing/run-local-tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# This script is run from outside of the docker environment and is useful for running
-# local tests.
+# This script is run from outside of the docker environment
 set -eu
 
 HUNTSMAN_DRP_COVDIR=${HUNTSMAN_DRP_COVDIR:-${HUNTSMAN_DRP}/coverage}
@@ -13,8 +12,12 @@ function cleanup {
 
 mkdir -p ${HUNTSMAN_DRP_COVDIR} && chmod -R 777 ${HUNTSMAN_DRP_COVDIR}
 
+echo "Building new docker image for testing..."
+docker-compose -f ${COMPOSE_FILE} build python-tests
+
+echo "Running python tests inside docker container..."
 trap cleanup EXIT
 docker-compose -f ${COMPOSE_FILE} run --rm \
   -e "HUNTSMAN_COVERAGE=/opt/lsst/software/stack/coverage" \
   -v "${HUNTSMAN_DRP_COVDIR}:/opt/lsst/software/stack/coverage" \
-  python_tests
+  python-tests
