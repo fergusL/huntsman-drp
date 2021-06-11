14153f752ec71284949ab9a7964bafc7f2d8d27d
danjampro <danjampro@sky.com>
2021-06-11 19:48:27 +1000

    Fix screener docker startup (#164)
    * first
    
    * update bash
    
    * update docker
    
    * update docker

--- docker/ingestor/bash-config.sh
+++ docker/ingestor/bash-config.sh
@@ -3,6 +3,3 @@ alias python=python3
 
 # need this so panoptes-utils knows where to find solve-field executable
 export SOLVE_FIELD=$(which solve-field)
-
-# activate the conda env
-conda activate screener
