f1501c9b9d3ab26f180b02fc278f51b589894bdc
danjampro <danjampro@sky.com>
2021-06-23 12:20:43 +1000

    Update run-file-ingestor.sh
    Fix file ingestor

--- scripts/run-file-ingestor.sh
+++ scripts/run-file-ingestor.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-source ${HUNTSMAN_DRP}/docker/bash-config.sh
+source ${HUNTSMAN_DRP}/docker/ingestor/bash-config.sh
 ${HUNTSMAN_DRP}/scripts/run-service huntsman.drp.services.ingestor.FileIngestor
